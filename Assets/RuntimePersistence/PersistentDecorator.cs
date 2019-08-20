8dffe216a93a01d6306b470c2eb46f775911b5f0
Maf'j Alvarez <jay@rootinteractive.com>
2019-08-21 00:55:00 +0100

    [Paul Hayes] Added Holons
    Holons are objects that persist in the scene in Editor and Runtime. Thanks a million Paul!

---
+++ Assets/RuntimePersistence/PersistentDecorator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersistentDecorator : MonoBehaviour
+{
+    [SerializeField]
+    Component target;
+
+    void SetData(string data){
+
+
+        JsonUtility.FromJsonOverwrite(data,target);
+    }
+
+    string GetData(){
+        
+        return JsonUtility.ToJson(target);
+    }
+}
