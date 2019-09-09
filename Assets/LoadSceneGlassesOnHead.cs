35578633adf3de3ac38c43afa7418de50516f2fe
Maf'j Alvarez <jay@rootinteractive.com>
2019-09-09 16:41:19 +0100

    added script to attach glasses to headset

--- Assets/LoadSceneGlassesOnHead.cs
+++ Assets/LoadSceneGlassesOnHead.cs
@@ -15,11 +15,38 @@ void Start()
     {
     }
 
+    private void OnTriggerEnter(Collider other)
+    {
+       //Debug.Log("just collided with " + other);
+       if(other.name == "headset"){
+           Debug.Log("this is the headset");
+           attachGlassesToHead();
+       }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+       dettachGlassesFromHead();
+    }
+
+    private void attachGlassesToHead()
+    {
+        Debug.Log("attached from headset");
+        this.GetComponent<Rigidbody>().isKinematic = true;
+        this.transform.SetParent(headset.transform);
+        this.transform.localPosition = Vector3.zero;
+        this.transform.localRotation = Quaternion.Euler(90f, 0f, 0f);        
+    }
+
+    private void dettachGlassesFromHead()
+    {
+        Debug.Log("detached from headset");
+        this.GetComponent<Rigidbody>().isKinematic = false;       
+    }
+
     public void LoadScene(string sceneToLoad)
     {
         Debug.Log("sceneName to load: " + sceneToLoad);
-        this.transform.SetParent(headset.transform);
-        this.transform.SetPositionAndRotation(headset.transform.position, headset.localRotation);
         SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
     }
 }
