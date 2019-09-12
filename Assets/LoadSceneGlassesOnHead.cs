a071bc7a8ddf4c03fd779204e9741769ec3cefdf
Maf'j Alvarez <jay@rootinteractive.com>
2019-09-12 11:43:53 +0100

    adding stans eyes texture for camera tween nodes

--- Assets/LoadSceneGlassesOnHead.cs
+++ Assets/LoadSceneGlassesOnHead.cs
@@ -1,34 +1,22 @@
-﻿// SceneA.
-// SceneA is given the sceneName which will
-// load SceneB from the Build Settings
-
+﻿
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using Valve.VR;
 
 public class LoadSceneGlassesOnHead : MonoBehaviour
 {
 
-    
-    public string sceneToLoad;
-    
-    public string homeScene;
-
-        private void OnTriggerEnter(Collider other)
-        {
-        //Debug.Log("just collided with " + other);
-        if(other.name == "HeadCollider"){
-            Debug.Log("this is the headset");
-            this.transform.parent = null;
-            Destroy(this.gameObject);
-            SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
-            }
-        }
-
-
-        private void OnTriggerExit(Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-            if(other.name == "HeadCollider"){
-            Debug.Log("detached from headset");
+    //Debug.Log("just collided with " + other);
+    if(other.name == "HeadCollider"){
+        Debug.Log("this is the headset");
+        if(gameObject.GetComponent<SteamVR_LoadLevel>()!= null){
+            this.gameObject.GetComponent<SteamVR_LoadLevel>().Trigger();
+            }
+        else Debug.LogError("You are missing a SteamVR_LoadLevel component");
         }
     }
 }
